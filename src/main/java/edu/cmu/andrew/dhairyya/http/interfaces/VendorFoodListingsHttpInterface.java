80ccd0ed5dfc2a222deed1cdccf664addcae5bce
Ritu Rao <ritu.rao@west.cmu.edu>
2019-11-30 17:40:25 -0800

    Food Listing added

--- src/main/java/edu/cmu/andrew/dhairyya/http/interfaces/VendorFoodListingsHttpInterface.java
+++ src/main/java/edu/cmu/andrew/dhairyya/http/interfaces/VendorFoodListingsHttpInterface.java
@@ -1,4 +1,111 @@
 package edu.cmu.andrew.dhairyya.http.interfaces;
 
-public class VendorFoodListingsHttpInterface {
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import edu.cmu.andrew.dhairyya.http.exceptions.HttpBadRequestException;
+import edu.cmu.andrew.dhairyya.http.responses.AppResponse;
+import edu.cmu.andrew.dhairyya.http.utils.PATCH;
+import edu.cmu.andrew.dhairyya.managers.VendorFoodListingsManager;
+import edu.cmu.andrew.dhairyya.managers.VendorManager;
+import edu.cmu.andrew.dhairyya.models.FoodListings;
+import edu.cmu.andrew.dhairyya.models.Vendor;
+import edu.cmu.andrew.dhairyya.utils.AppLogger;
+import org.json.JSONObject;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
+
+@Path("/FoodItems")
+public class VendorFoodListingsHttpInterface extends HttpInterface {
+
+    private ObjectWriter ow;
+
+    public VendorFoodListingsHttpInterface() {
+        ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+    }
+
+    @POST
+    @Consumes({MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_JSON})
+
+    public AppResponse insertFoodListings(Object request){
+
+        try{
+            JSONObject json = new JSONObject(ow.writeValueAsString(request));
+
+            FoodListings newFoodListings = new FoodListings(
+                    null,
+                    json.getString("vendorId"),
+                    json.getString("foodListingId"),
+                    json.getString("foodItemName"),
+                    json.getInt("quantityOfItem"),
+                    json.getDouble("pricePerMeal"),
+                    json.getDouble("caloriesPerMeal"),
+                    json.getString("keyIngredients"),
+                    json.getString("dayOfTheWeek")
+            );
+            VendorFoodListingsManager.getInstance().createVendorFoodListings(newFoodListings);
+            return new AppResponse("Insert Successful");
+
+        }catch (Exception e){
+            throw handleException("POST vendor food listings", e);
+        }
+    }
+
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public AppResponse getFoodListings(@Context HttpHeaders headers){
+
+        try{
+            AppLogger.info("Got an API call");
+            ArrayList<FoodListings> foodListings = VendorFoodListingsManager.getInstance().getVendorFoodListings();
+
+            if(foodListings != null)
+                return new AppResponse(foodListings);
+            else
+                throw new HttpBadRequestException(0, "Problem with getting food listings");
+        }catch (Exception e){
+            throw handleException("GET /FoodItems", e);
+        }
+    }
+
+    @GET
+    @Path("/{vendorId}")
+    @Produces({MediaType.APPLICATION_JSON})
+    public AppResponse getFoodListingsByVendorId(@Context HttpHeaders headers,@PathParam("vendorId") String vendorId){
+
+        try{
+            AppLogger.info("Got an API call");
+            ArrayList<FoodListings> foodListings = VendorFoodListingsManager.getInstance().getFoodListingByVendorId(vendorId);
+
+            if(foodListings != null)
+                return new AppResponse(foodListings);
+            else
+                throw new HttpBadRequestException(0, "Problem with getting food listings by Vendor ID");
+        }catch (Exception e){
+            throw handleException("GET /FoodItems/{vendorId}", e);
+        }
+    }
+
+    @GET
+    @Path("/{foodItemId}")
+    @Produces({MediaType.APPLICATION_JSON})
+    public AppResponse getFoodListingsByFoodItemId(@Context HttpHeaders headers,@PathParam("foodItemId") String foodItemId){
+
+        try{
+            AppLogger.info("Got an API call");
+            ArrayList<FoodListings> foodListings = VendorFoodListingsManager.getInstance().getFoodListingByFoodId(foodItemId);
+
+            if(foodListings != null)
+                return new AppResponse(foodListings);
+            else
+                throw new HttpBadRequestException(0, "Problem with getting food listings by Food ID");
+        }catch (Exception e){
+            throw handleException("GET /FoodItems/{foodId}", e);
+        }
+    }
 }
--- src/main/java/edu/cmu/andrew/dhairyya/models/FoodListings.java
+++ src/main/java/edu/cmu/andrew/dhairyya/models/FoodListings.java
@@ -1,4 +1,98 @@
 package edu.cmu.andrew.dhairyya.models;
 
 public class FoodListings {
+    private String id ;
+    private String vendorId;
+    private String foodListingId;
+    private String foodItemName;
+    private int quantityOfItem;
+    private double pricePerMeal;
+    private double caloriesPerMeal;
+    private String keyIngredients;
+    private String dayOfTheWeek;
+
+    public FoodListings(String id, String vendorId, String foodListingId, String foodItemName, int quantityOfItem, double pricePerMeal, double caloriesPerMeal, String keyIngredients, String dayOfTheWeek) {
+        this.id = id;
+        this.vendorId = vendorId;
+        this.foodListingId = foodListingId;
+        this.foodItemName = foodItemName;
+        this.quantityOfItem = quantityOfItem;
+        this.pricePerMeal = pricePerMeal;
+        this.caloriesPerMeal = caloriesPerMeal;
+        this.keyIngredients = keyIngredients;
+        this.dayOfTheWeek = dayOfTheWeek;
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getVendorId() {
+        return vendorId;
+    }
+
+    public void setVendorId(String vendorId) {
+        this.vendorId = vendorId;
+    }
+
+    public String getFoodListingId() {
+        return foodListingId;
+    }
+
+    public void setFoodListingId(String foodListingId) {
+        this.foodListingId = foodListingId;
+    }
+
+    public String getFoodItemName() {
+        return foodItemName;
+    }
+
+    public void setFoodItemName(String foodItemName) {
+        this.foodItemName = foodItemName;
+    }
+
+    public int getQuantityOfItem() {
+        return quantityOfItem;
+    }
+
+    public void setQuantityOfItem(int quantityOfItem) {
+        this.quantityOfItem = quantityOfItem;
+    }
+
+    public double getPricePerMeal() {
+        return pricePerMeal;
+    }
+
+    public void setPricePerMeal(double pricePerMeal) {
+        this.pricePerMeal = pricePerMeal;
+    }
+
+    public double getCaloriesPerMeal() {
+        return caloriesPerMeal;
+    }
+
+    public void setCaloriesPerMeal(double caloriesPerMeal) {
+        this.caloriesPerMeal = caloriesPerMeal;
+    }
+
+    public String getKeyIngredients() {
+        return keyIngredients;
+    }
+
+    public void setKeyIngredients(String keyIngredients) {
+        this.keyIngredients = keyIngredients;
+    }
+
+    public String getDayOfTheWeek() {
+        return dayOfTheWeek;
+    }
+
+    public void setDayOfTheWeek(String dayOfTheWeek) {
+        this.dayOfTheWeek = dayOfTheWeek;
+    }
 }
